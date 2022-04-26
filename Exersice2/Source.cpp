fc1426b1288158d408d1644c66bc7b29ee0643cd
alexandr shishigin <iamwer4@gmail.com>
2022-04-26 16:42:53 +0900

    Добавьте файлы проекта.

---
+++ Exersice2/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <locale>
+
+int main()
+{
+	char chLetter('a');
+
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "Введите букву нижнего регистра:\n\n";
+
+	std::cin >> chLetter;
+
+	chLetter = chLetter - 32;
+
+	std::cout << "\nТа же буква в верхнем регистре: " << chLetter << ".\n\n";
+
+	return 0;
+}
